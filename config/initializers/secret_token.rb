9a08ffd7b9c82b49090acfb974decf3c3217247a
Seyit Ali Serbest <dmadmin@samanyolu.tv>
2013-06-28 14:29:21 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trtir::Application.config.secret_key_base = 'ab0c4392792bbe40bdd0b0df119fb4c7c116ada19f9946e9ab40fb9c15bf1f326168d10693144fe819696688e2cd450249b411e71cf57147eeb8caf324a51fb2'
