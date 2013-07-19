81705d9e4198c688f6c8bf7a22087fbe70f5b5a7
Seyit Ali Serbest <dmadmin@samanyolu.tv>
2013-07-20 01:17:50 +0300

    spec utilities

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,3 @@
-def full_title(page_title)
-  base_title = "Türkiye'nin yeni sosyal medya ortamı"
-  if page_title.empty?
-    base_title
-  else
-    "#{base_title} | #{page_title}"
-  end
-end
+
+
+include ApplicationHelper
